5162b21daf8170452e68556372a9e2d3ec84f4ea
Nicoyeuiem <raidensogay@gmail.com>
2026-06-25 13:29:22 +0700

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,12 +1,15 @@
-
-#include "User.h"
+// user.cpp
+#include "user.h"
 #include <iostream>
 
 User::User(std::string u, std::string p) : username(u), password(p) {}
 std::string User::getUsername() { return username; }
+bool User::checkPassword(std::string p) { return password == p; }
 
-Customer::Customer(std::string u, std::string p) : User(u, p) {}
+Customer::Customer(std::string u, std::string p) : User(u, p), address("Chua co dia chi") {}
 void Customer::displayRole() { std::cout << "Role: Customer\n"; }
+void Customer::setAddress(std::string addr) { address = addr; }
+std::string Customer::getAddress() { return address; }
 
 Admin::Admin(std::string u, std::string p) : User(u, p) {}
 void Admin::displayRole() { std::cout << "Role: Admin\n"; }
