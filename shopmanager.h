f3df69aab700f620b318f8bfbb175256430e3db0
Nicoyeuiem <raidensogay@gmail.com>
2026-06-25 13:29:57 +0700

    Update shopmanager.h

--- shopmanager.h
+++ shopmanager.h
@@ -1,4 +1,4 @@
-// ShopManager.h
+// shopmanager.h
 #ifndef SHOPMANAGER_H
 #define SHOPMANAGER_H
 
@@ -10,13 +10,19 @@
 class ShopManager {
 private:
     std::vector<Product*> products;
+    std::vector<Customer*> registeredCustomers;
     Cart currentCart;
     Customer* currentCustomer;
 public:
     ShopManager();
     ~ShopManager();
     void setupShop();
-    void displayMenu();
+    void start();
+    void authMenu();
+    void mainMenu();
+    void registerAccount();
+    void loginAccount();
+    void updateAddress();
 };
 
 #endif
